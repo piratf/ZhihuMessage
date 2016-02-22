87936b0db248ae81f6b54c109d7f7601e5ef7d1e
piratf <948970803@qq.com>
2016-02-22 15:27:02 +0800

    完成 小红点提醒，注册了 alarm 和 oninstalled 相关事件

---
+++ js/eventPage.js
@@ -0,0 +1,81 @@
+chrome.runtime.onStartup.addListener(function() {
+    console.log("zhihu message started")
+    onInit()
+})
+
+chrome.runtime.onInstalled.addListener(onInit);
+chrome.alarms.onAlarm.addListener(onAlarm);
+
+function onInit() {
+    // 5 分钟查一次
+    chrome.alarms.create("zhihuMessage", { when: Date.now(), periodInMinutes: 1 });
+}
+
+function onAlarm(alarm) {
+    console.log('Got alarm', alarm);
+    if (alarm && alarm.name == "zhihuMessage") {
+        parseZhihuMessage();
+    }
+}
+
+function parseZhihuMessage(alarm) {
+    console.log('Got alarm', alarm);
+    // 这是单独的消息计数接口
+    var targetLink_new = "https://www.zhihu.com/noti7/new"
+    var argument = { r: new Date().getTime() };
+
+    // 获取消息计数
+    myCORS_get(targetLink_new, argument, function(responseText) {
+        var resultJSON = JSON.parse(responseText);
+        // console.log(resultJSON[0])
+        // 回复报文的第二个对象是消息计数
+        var msgCnt = resultJSON[1]
+        var msgCntSum = msgCnt.reduce(function(previous, current, index, array) {
+            return previous + current;
+        });
+
+        // 有新消息
+        if (msgCntSum > 0) {
+            chrome.browserAction.setIcon({path: "./icon3.png"})
+        } else {
+            chrome.browserAction.setIcon({path: "./icon2.png"})
+        }
+    });
+}
+
+function createCORSRequest(method, url) {
+    var xhr = new XMLHttpRequest();
+    if ("withCredentials" in xhr) {
+        // Check if the XMLHttpRequest object has a "withCredentials" property.
+        // "withCredentials" only exists on XMLHTTPRequest2 objects.
+        xhr.open(method, url, true);
+    } else if (typeof XDomainRequest != "undefined") {
+        // Otherwise, check if XDomainRequest.
+        // XDomainRequest only exists in IE, and is IE's way of making CORS requests.
+        xhr = new XDomainRequest();
+        xhr.open(method, url);
+    } else {
+        // Otherwise, CORS is not supported by the browser.
+        xhr = null;
+    }
+    return xhr;
+}
+
+function myCORS_get(url, argument, func) {
+    var xhr = createCORSRequest('GET', url);
+    if (!xhr) {
+        throw new Error('CORS not supported');
+    }
+
+    xhr.onload = function() {
+        func(xhr.responseText)
+    }
+
+    xhr.onerror = function() {
+        console.log('There was an error!');
+    };
+
+    xhr.withCredentials = true;
+
+    xhr.send(argument);
+}
--- js/getNotice.js
+++ js/getNotice.js
@@ -1,5 +1,11 @@
 $(document).ready(getNotice)
 
+$("#mail").bind("click", function() {
+    var createProperties = new Object();;
+    createProperties.url = "https://www.zhihu.com/inbox";
+    chrome.tabs.create(createProperties);
+})
+
 function createCORSRequest(method, url) {
     var xhr = new XMLHttpRequest();
     if ("withCredentials" in xhr) {
@@ -37,10 +43,110 @@ function myCORS_get(url, argument, func) {
     xhr.send(argument);
 }
 
-function zhihuLinkHead() {
-
+// 为消息中的链接完善地址
+function zhihuLinkHead(htmlDOM, option) {
+    $(option, htmlDOM).each(function() {
+        var rawLink = $(this).attr('href')
+        var newLink = 'https://www.zhihu.com' + rawLink
+        $(this).attr('href', newLink);
+        $(this).bind("click", function() {
+                var createProperties = new Object();;
+                createProperties.url = newLink;
+                chrome.tabs.create(createProperties);
+            })
+            // console.log(rawLink)
+    });
 }
 
+function parseZhihuNotice(htmlDOM) {
+    zhihuLinkHead(htmlDOM, "a.zg-link")
+    zhihuLinkHead(htmlDOM, "a.question_link")
+
+    // 取出数据中有用的 span 信息
+    divs = []
+    $(htmlDOM).each(function(i, dv) {
+        divs[i] = dv
+    })
+
+    // console.log(divs)
+
+    // 创建对象
+    $("section#noticeSection").html("")
+
+    for (var i = 0; i < divs.length; i += 2) {
+        $("section#noticeSection").append($("<div>", {
+            class: "item"
+        }).append($("<div>", {
+            class: "item-content"
+        }).append($("<div>", {
+            class: "text"
+        }).append(divs[i]))))
+    }
+
+    return htmlDOM
+}
+
+function parseZhihuFollow(htmlDOM) {
+    zhihuLinkHead(htmlDOM, "a.zm-item-link-avatar")
+
+    // 取出数据中有用的 span 信息
+    divs = []
+    $(htmlDOM).each(function(i, dv) {
+        divs[i] = dv
+    })
+
+    links = []
+    $("a.zm-item-link-avatar", htmlDOM).each(function(i, link) {
+        links[i] = link
+    })
+
+    // console.log(links)
+
+    // 创建对象
+
+    for (var i = 0, j = 0; i < divs.length; i += 2, ++j) {
+        $("section#followSection").append($("<div>", {
+            class: "item"
+        }).append($("<div>", {
+            class: "item-content"
+        }).append($("<div>", {
+            class: "media pull-left"
+        }).html(links[j]), $("<div>", {
+            class: "text"
+        }).append(divs[i]))))
+    }
+
+    return htmlDOM
+}
+
+function parseZhihuVoteThank(htmlDOM) {
+    zhihuLinkHead(htmlDOM, "a.zg-link")
+    zhihuLinkHead(htmlDOM, "a.question_link")
+
+    // 取出数据中有用的 span 信息
+    divs = []
+    $(htmlDOM).each(function(i, div) {
+        divs[i] = div
+    })
+
+    // console.log(divs)
+
+    // 创建对象
+
+    for (var i = 0; i < divs.length; i += 2) {
+        $("section#voteThankSection").append($("<div>", {
+            class: "item"
+        }).append($("<div>", {
+            class: "item-content"
+        }).append($("<div>", {
+            class: "text"
+        }).append(divs[i]))))
+    }
+
+    return htmlDOM
+}
+
+// 向 zhihu.com 发送请求，获取信息的主函数
 function getNotice() {
     var timestamp = new Date().getTime();
     var argument = { r: timestamp };
@@ -48,44 +154,24 @@ function getNotice() {
     var targetLink_follow = "https://www.zhihu.com/noti7/stack/follow";
     var targetLink_vote_thank = "https://www.zhihu.com/noti7/stack/vote_thank";
 
-    var msgCnt = 0
-
+    // 获取普通消息
     myCORS_get(targetLink_default, argument, function(responseText) {
         var resultJSON = JSON.parse(responseText);
-        msgCnt += resultJSON['r'];
         var noticeHTML = jQuery.parseHTML(resultJSON['msg']);
-
-        $("a.zg-link", noticeHTML).each(function() {
-            rawLink = $(this).attr('href')
-            $(this).attr('href', 'https://www.zhihu.com' + rawLink);
-            // console.log(rawLink)
-        });
-
-        $("a.question_link", noticeHTML).each(function() {
-            rawLink = $(this).attr('href')
-            $(this).attr('href', 'https://www.zhihu.com' + rawLink);
-            // console.log(rawLink)
-        });
-        // console.log(resultJSON['msg']);
-        $("div#notice").html(noticeHTML);
+        parseZhihuNotice(noticeHTML)
     });
 
+    // 获取关注消息
     myCORS_get(targetLink_follow, argument, function(responseText) {
         var resultJSON = JSON.parse(responseText);
-        msgCnt += resultJSON['r'];
-        // console.log(resultJSON['msg']);
-        $("div#follow").html(resultJSON['msg']);
+        var followHTML = jQuery.parseHTML(resultJSON['msg']);
+        parseZhihuFollow(followHTML)
     });
 
+    // 获取点赞消息
     myCORS_get(targetLink_vote_thank, argument, function(responseText) {
         var resultJSON = JSON.parse(responseText);
-        msgCnt += resultJSON['r'];
-        // console.log(resultJSON['msg']);
-        $("div#vote_thank").html(resultJSON['msg']);
+        var voteThankHTML = jQuery.parseHTML(resultJSON['msg']);
+        parseZhihuVoteThank(voteThankHTML)
     });
-
-
-    if (msgCnt > 0) {
-        console.log("new message", msgCnt);
-    }
 }
